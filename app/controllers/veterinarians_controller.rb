5273a22cb07dda02843263c10430ea00969f8244
Nestor Castillo <nes_23@yahoo.com>
2016-04-12 17:18:30 -0600

    Add create method to VeterinariansController

--- app/controllers/veterinarians_controller.rb
+++ app/controllers/veterinarians_controller.rb
@@ -1,11 +1,22 @@
 class VeterinariansController < ApplicationController
   def new
     @veterinarian = Veterinarian.new
-    @veterinarian.build_address
-
+    @address = @veterinarian.build_address
   end
 
   def create
-    redirect_to root_path
+    @veterinarian = Veterinarian.new(vet_params)
+
+    if @veterinarian.save
+      redirect_to root_path, notice: "Veterinarian info added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def vet_params
+    params.require(:veterinarian).permit(:organization_name)
   end
 end
--- spec/controllers/veterinarians_controller_spec.rb
+++ spec/controllers/veterinarians_controller_spec.rb
@@ -11,7 +11,25 @@ describe VeterinariansController, type: :controller do
     it "renders new template" do
       get :new
 
-      expect(response).to render_template :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "it redirects to index page", focus: true do
+        post :create, veterinarian: { organization_name: "The Animal Hospital" }
+
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "re renders the form" do
+        post :create, veterinarian: { organization_name: nil }
+
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
