1321433c69554d567b229aa78b481afc51380147
Nestor Castillo <nes_23@yahoo.com>
2016-04-12 17:15:59 -0600

    Add more validations to Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
+
+  validates :address_1, :city, :state, :zip_code, presence: true
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 describe Address do
-  it { is_expected.to belong_to(:addressable) }
+  describe "Associations" do
+    it { is_expected.to belong_to(:addressable) }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:address_1) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:zip_code) }
+  end
 end
