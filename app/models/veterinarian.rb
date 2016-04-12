41002854d0713b73065ed463d7d081be70b597d4
Nestor Castillo <nes_23@yahoo.com>
2016-04-12 17:16:48 -0600

    Add validations to Veterinarian

--- app/models/veterinarian.rb
+++ app/models/veterinarian.rb
@@ -1,4 +1,6 @@
 class Veterinarian < ActiveRecord::Base
   has_one :address, as: :addressable
   accepts_nested_attributes_for :address
+
+  validates :organization_name, presence: true
 end
--- spec/models/veterinarian_spec.rb
+++ spec/models/veterinarian_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 describe Veterinarian do
   it { is_expected.to have_one(:address) }
   it { is_expected.to accept_nested_attributes_for(:address) }
+  it { is_expected.to validate_presence_of(:organization_name) }
 end
